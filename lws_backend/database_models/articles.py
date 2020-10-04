1a54fa73dc55ba47cffefcbeae37b7aafa6839f1
Alex Korzh <alexkorzh7@gmail.com>
2020-10-04 16:44:53 +0200

    Add database models

---
+++ lws_backend/database_models/articles.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Text
+from sqlalchemy.orm import relationship
+
+from fastapi_test.database import Base
+from fastapi_test.models.icon import Icon
+
+
+class ArticleBase(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True), nullable=False)
+    deleted_at = Column(DateTime(timezone=True))
+    reference_id = Column(String, nullable=False)
+    name = Column(String)
+    subtitle = Column(String)
+    category = Column(Integer, default=0)
+
+
+class Article(ArticleBase):
+    article_text = Column(Text)
+
+
+class ArticlePreview(ArticleBase):
+    icon_id = Column(Integer, ForeignKey("icon.id"))
+
+    icon = relationship("Icon", lazy="joined")
---
+++ lws_backend/database_models/icon.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from fastapi_test.database import Base
+
+
+class Icon(Base):
+    __tablename__ = "icons"
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True), nullable=False)
+    deleted_at = Column(DateTime(timezone=True))
+    data = Column(String)
+    height = Column(String)
+    width = Column(String)
