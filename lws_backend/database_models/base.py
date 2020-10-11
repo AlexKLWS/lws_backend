20e28686b018c22a9f0f928caa4c7a1ffaeac3eb
Alex Korzh <alexkorzh7@gmail.com>
2020-10-11 23:33:50 +0200

    Update default values for db model base class

--- lws_backend/database_models/base.py
+++ lws_backend/database_models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import func, Column, Integer, DateTime
 
 from lws_backend.database import Base
 
@@ -7,6 +7,7 @@ class DatabaseBaseModel(Base):
     __abstract__ = True
 
     id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime(timezone=True), nullable=False)
-    updated_at = Column(DateTime(timezone=True), nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False,
+                        server_default=func.now(), server_onupdate=func.now())
     deleted_at = Column(DateTime(timezone=True))
