6b548c840f83b5a4d28aba0d008dcb60981ba034
Alex Korzh <alexkorzh7@gmail.com>
2020-10-11 22:40:34 +0200

    Add authorization check dependancy

---
+++ lws_backend/api/dependencies/authorization.py
@@ -0,0 +1,28 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
+from lws_backend.main import config
+from lws_backend.core.config import JWT_ENCODE_SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+async def check_user_auth(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, config.get(JWT_ENCODE_SECRET_KEY), algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    # user = get_user(fake_users_db, username=username)
+    user = None
+    if user is None:
+        raise credentials_exception
+    return user
