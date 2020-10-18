900f597500c9c207a9bc27c7c1ff7b970a195ef4
Alex Korzh <alexkorzh7@gmail.com>
2020-10-18 21:59:51 +0200

    Extend page index db model to have jsonified dict conversion methods

--- lws_backend/database_models/page_index.py
+++ lws_backend/database_models/page_index.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, DateTime
+from lws_backend.pydantic_models.page_index import PageIndex as PageIndexJsonified
 
 from lws_backend.database_models.base import DatabaseBaseModel
 
@@ -10,3 +11,19 @@ class PageIndex(DatabaseBaseModel):
     end_date = Column(DateTime(timezone=True), nullable=False)
     page = Column(Integer)
     category = Column(Integer, default=0)
+
+    def get_jsonified_dict(self):
+        return {
+            "createdAt": self.created_at,
+            "startDate": self.start_date,
+            "endDate": self.end_date,
+            "page": self.page,
+            "category": self.category,
+        }
+
+    def from_jsonified_dict(self, p: PageIndexJsonified):
+        self.created_at = p.createdAt
+        self.start_date = p.startDate
+        self.end_date = p.endDate
+        self.category = p.category
+        return self
