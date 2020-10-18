1272c41c9998e1b269b8b9aff047997a09893329
Alex Korzh <alexkorzh7@gmail.com>
2020-10-18 15:50:41 +0200

    Make it so updated at column is set automatically

--- lws_backend/database_models/base.py
+++ lws_backend/database_models/base.py
@@ -9,5 +9,5 @@ class DatabaseBaseModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = Column(DateTime(timezone=True), nullable=False,
-                        server_default=func.now(), server_onupdate=func.now())
+                        server_default=func.now(), onupdate=func.now())
     deleted_at = Column(DateTime(timezone=True))
