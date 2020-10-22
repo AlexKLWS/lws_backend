39031d4e8918e69f8e189e126be9a91f3a263c49
Alex Korzh <alexkorzh7@gmail.com>
2020-10-23 01:04:55 +0200

    Refactor and fix files routes a bit

--- lws_backend/api/routes/files.py
+++ lws_backend/api/routes/files.py
@@ -6,6 +6,9 @@ from lws_backend.files import add_file_metadata, pop_file_metadata
 from lws_backend.api.dependencies.authorization import check_user_auth
 from lws_backend.pydantic_models.user_access_rights import UserAccessRights
 from lws_backend.pydantic_models.file_metadata import FileMetadataPostRequest
+from lws_backend.core.config import (
+    ASSETS_PATH,
+)
 
 router = APIRouter()
 
@@ -14,6 +17,7 @@ router = APIRouter()
 async def add_metadata(metadataRequest: FileMetadataPostRequest, access_rights=Depends(check_user_auth)):
     if access_rights != UserAccessRights.WRITE:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User doesn't have required access rights")
+
     for metadata in metadataRequest.metaData:
         metadata.referenceId = str(uuid.uuid4())
     add_file_metadata(metadataRequest)
@@ -25,13 +29,16 @@ async def add_files(referenceId: str = Form(...), file: UploadFile = File(...),
                     access_rights=Depends(check_user_auth)):
     if access_rights != UserAccessRights.WRITE:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User doesn't have required access rights")
+
     file_metadata = pop_file_metadata(referenceId)
-    dir = os.path.join("assets", file_metadata.folder)
-    if not os.path.exists(dir):
-        os.makedirs(dir, exist_ok=True)
+    dir = ASSETS_PATH
+    if not file_metadata.folder:
+        dir = os.path.join(dir, file_metadata.folder)
+        if not os.path.exists(dir):
+            os.makedirs(dir, exist_ok=True)
 
     file_format = file.filename.split(".")[-1]
-    file_name = file_metadata.newName if file_metadata.newName + "." + file_format is not None else file.filename
+    file_name = file_metadata.newName + "." + file_format if file_metadata.newName is not None else file.filename
 
     file_path = os.path.join(dir, file_name)
 
