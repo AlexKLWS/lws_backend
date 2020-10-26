d8944d5eac22a9cc356af5ebb71df1d087f62065
Alex Korzh <alexkorzh7@gmail.com>
2020-10-26 23:32:32 +0100

    Integrate guide routes with the rest of the app

--- lws_backend/api/routes/api.py
+++ lws_backend/api/routes/api.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from lws_backend.api.routes import articles, previews, ext_materials, login, files
+from lws_backend.api.routes import articles, previews, ext_materials, login, files, guides
 
 router = APIRouter()
 
 router.include_router(articles.router, tags=["articles"], prefix="/articles")
 router.include_router(previews.router, tags=["previews"], prefix="/previews")
 router.include_router(ext_materials.router, tags=["ext_materials"], prefix="/ext-materials")
+router.include_router(guides.router, tags=["guides"], prefix="/guides")
 router.include_router(login.router, tags=["login"], prefix="/login")
 router.include_router(files.router, tags=["files"], prefix="/files")
