03cce57110229077b868615a41a729eebd317745
Alex Korzh <alexkorzh7@gmail.com>
2020-10-29 01:23:34 +0100

    Check token if token is expired before authorizing

--- lws_backend/api/dependencies/authorization.py
+++ lws_backend/api/dependencies/authorization.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime
 from jose import JWTError, jwt
 
 from lws_backend.config import config
@@ -19,10 +20,17 @@ async def check_user_auth(token: str = Depends(oauth2_scheme), db: Session = Dep
     try:
         payload = jwt.decode(token, config.get(JWT_ENCODE_SECRET_KEY), algorithms=[ALGORITHM])
         username: str = payload.get("sub")
+        expiry_date: int = payload.get("exp")
         if username is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
+    if datetime.utcfromtimestamp(expiry_date) < datetime.utcnow():
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     user = get_user_by_username(db, username)
     if user is None:
         raise credentials_exception
