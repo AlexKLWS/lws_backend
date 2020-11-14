dd375011ad6892570b99d49e43357525fce76089
Alex Korzh <alexkorzh7@gmail.com>
2020-11-15 00:11:07 +0100

    Refactor articles crud functions

--- lws_backend/crud/articles.py
+++ lws_backend/crud/articles.py
@@ -2,8 +2,8 @@ from typing import List
 from sqlalchemy import and_
 
 from lws_backend.database import Session
-from lws_backend.database_models.categories import Category as DBCategory
-from lws_backend.pydantic_models.category import Category
+from lws_backend.database_models.categories import Category
+from lws_backend.pydantic_models.category import Category as CategoryENUM
 from lws_backend.database_models.page_index import PageIndex
 from lws_backend.database_models.articles import Article, ArticlePreview
 from lws_backend.database_models.icons import Icon
@@ -11,13 +11,13 @@ from lws_backend.pydantic_models.article import Article as ArticleJsonified
 
 
 def get_article_by_id(db: Session, id: str) -> Article:
-    return db.query(Article).filter(Article.reference_id == id).first()
+    return db.query(Article).filter_by(reference_id=id).first()
 
 
 def get_article_previews(
-    db: Session, page_index: PageIndex, category: Category
+    db: Session, page_index: PageIndex, category: CategoryENUM
 ) -> List[ArticlePreview]:
-    category_record = db.query(DBCategory).filter(DBCategory.enum_value == category.value).first()
+    category_record = db.query(Category).filter_by(enum_value=category.value).first()
     if category_record is None:
         return []
     if page_index.page == 1:
@@ -34,9 +34,9 @@ def get_article_previews(
 def upsert_article(db: Session, article_jsonified: ArticleJsonified):
     categories = []
     for category in article_jsonified.categories:
-        category_record = db.query(DBCategory).filter(DBCategory.enum_value == category.value).first()
+        category_record = db.query(Category).filter_by(enum_value=category.value).first()
         if category_record is None:
-            category_record = DBCategory(enum_value=category.value)
+            category_record = Category(enum_value=category.value)
             db.add(category_record)
         categories.append(category_record)
 
