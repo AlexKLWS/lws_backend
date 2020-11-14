c3e30f9655406d286831348835cd7145dedf141d
Alex Korzh <alexkorzh7@gmail.com>
2020-11-14 20:33:21 +0100

    Update articles to use new categories

--- lws_backend/api/routes/articles.py
+++ lws_backend/api/routes/articles.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 import uuid
 
 from lws_backend.database import Session, get_db
+from lws_backend.crud.managed_session import managed_session
 from lws_backend.crud.articles import get_article_by_id
 from lws_backend.pydantic_models.article import Article
 from lws_backend.pydantic_models.category import Category
@@ -15,7 +16,9 @@ router = APIRouter()
 
 @router.get("", response_model=Article)
 async def get_article(id: str, db: Session = Depends(get_db)):
-    article = get_article_by_id(db, id)
+    with managed_session(db, True) as session:
+        article = get_article_by_id(session, id)
+
     if article is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Article not found")
 
@@ -30,10 +33,10 @@ async def add_or_update_article(article: Article, db: Session = Depends(get_db),
 
     if article.referenceId is None:
         article.referenceId = str(uuid.uuid4())
-    upsert_article(db, article)
+    article.categories.append(Category.MISC)
 
-    update_index(db, Category.MISC)
-    if article.category != Category.MISC:
-        update_index(db, article.category)
+    with managed_session(db, True) as session:
+        upsert_article(session, article)
+        update_index(session, article.categories)
 
     return article
--- lws_backend/crud/articles.py
+++ lws_backend/crud/articles.py
@@ -2,6 +2,7 @@ from typing import List
 from sqlalchemy import and_
 
 from lws_backend.database import Session
+from lws_backend.database_models.categories import Category as DBCategory
 from lws_backend.pydantic_models.category import Category
 from lws_backend.database_models.page_index import PageIndex
 from lws_backend.database_models.articles import Article, ArticlePreview
@@ -16,51 +17,38 @@ def get_article_by_id(db: Session, id: str) -> Article:
 def get_article_previews(
     db: Session, page_index: PageIndex, category: Category
 ) -> List[ArticlePreview]:
-    article_preview_filter = None
+    category_record = db.query(DBCategory).filter(DBCategory.enum_value == category.value).first()
     if page_index.page == 1:
-        article_preview_filter = (
-            and_(
-                ArticlePreview.created_at >= page_index.start_date,
-                ArticlePreview.category == category.value,
-            )
-            if category != Category.MISC
-            else and_(ArticlePreview.created_at >= page_index.start_date)
-        )
+        article_previews = category_record.articles.filter(
+            and_(ArticlePreview.created_at >= page_index.start_date, ArticlePreview.hidden.isnot(True))).all()
+        return article_previews
     else:
-        article_preview_filter = (
-            and_(
-                ArticlePreview.created_at >= page_index.start_date,
-                ArticlePreview.created_at <= page_index.end_date,
-                ArticlePreview.category == category.value,
-            )
-            if category != Category.MISC
-            else and_(
-                ArticlePreview.created_at >= page_index.start_date,
-                ArticlePreview.created_at <= page_index.end_date,
-            )
-        )
-
-    if article_preview_filter is not None:
-        article_previews = db.query(ArticlePreview).filter(article_preview_filter).all()
+        article_previews = category_record.articles.filter(and_(ArticlePreview.created_at >= page_index.start_date,
+                                                                ArticlePreview.created_at <= page_index.end_date,
+                                                                ArticlePreview.hidden.isnot(True))).all()
         return article_previews
     return []
 
 
 def upsert_article(db: Session, article_jsonified: ArticleJsonified):
-    try:
-        existing_record = db.query(Article).filter(Article.reference_id == article_jsonified.referenceId).first()
-        if existing_record is not None:
-            existing_record.from_jsonified_dict(article_jsonified)
-            existing_icon_record = db.query(Icon).filter(Icon.id ==
-                                                         existing_record.icon_id).first()
-            existing_icon_record.from_jsonified_dict(article_jsonified.icon)
-        else:
-            article = Article().from_jsonified_dict(article_jsonified)
-            article.icon = Icon().from_jsonified_dict(article_jsonified.icon)
-            db.add(article)
-        db.commit()
-    except:
-        db.rollback()
-        raise
-    finally:
-        db.close()
+    categories = []
+    # Removing duplicates
+    consolidated_categories = list(dict.fromkeys(article_jsonified.categories))
+    for category in consolidated_categories:
+        category_record = db.query(DBCategory).filter(DBCategory.enum_value == category.value).first()
+        if category_record is None:
+            category_record = Category(enum_value=category.value)
+            db.add(category_record)
+        categories.append(category_record)
+
+    existing_record = db.query(Article).filter(Article.reference_id == article_jsonified.referenceId).first()
+    if existing_record is not None:
+        existing_record.from_jsonified_dict(article_jsonified)
+        existing_icon_record = db.query(Icon).filter(Icon.id ==
+                                                     existing_record.icon_id).first()
+        existing_icon_record.from_jsonified_dict(article_jsonified.icon)
+    else:
+        article = Article().from_jsonified_dict(article_jsonified)
+        article.icon = Icon().from_jsonified_dict(article_jsonified.icon)
+        article.categories.extend(categories)
+        db.add(article)
