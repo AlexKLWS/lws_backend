42a3d2b4246811fe5afa8526537bf1ea735eac41
Alex Korzh <alexkorzh7@gmail.com>
2020-11-14 21:59:56 +0100

    Make created at an index

--- lws_backend/database_models/base.py
+++ lws_backend/database_models/base.py
@@ -7,7 +7,7 @@ class DatabaseBaseModel(Base):
     __abstract__ = True
 
     id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    created_at = Column(DateTime(timezone=True), nullable=False, index=True, server_default=func.now())
     updated_at = Column(DateTime(timezone=True), nullable=False,
                         server_default=func.now(), onupdate=func.now())
     deleted_at = Column(DateTime(timezone=True))
